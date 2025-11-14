d9ff3d3238bcf41b4a39f130ec8512c4ec9757a0
Copetim1 <twschoolb@gmail.com>
2025-11-14 18:26:39 +0000

    Finished LLDQ and LLS

--- LLDQ.hpp
+++ LLDQ.hpp
@@ -18,19 +18,46 @@ public:
     LLDQ() = default;
 
     // Core Insertion Operations
-    void pushFront(const T& item) override;
-    void pushBack(const T& item) override;
+    void pushFront(const T& item) override{
+        list.addHead(item);
+    }
+
+    void pushBack(const T& item) override{
+        list.addTail(item);
+    }
 
     // Core Removal Operations
-    T popFront() override;
-    T popBack() override;
+    T popFront() override{
+        if(list.getHead() == nullptr){
+            throw std::runtime_error("Empty");
+        }
+        
+        T value = list.getHead()->data;
+        list.removeHead();
+        return value;
+    }
+    T popBack() override{
+        if(list.getHead() == nullptr){
+            throw std::runtime_error("Empty");
+        }
+        
+        T value = list.getTail()->data;
+        list.removeTail();
+        return value;
+    }
 
     // Element Accessors
-    const T& front() const override;
-    const T& back() const override;
+    const T& front() const override{
+        return list.getHead()->data;
+    }
+    const T& back() const override{
+        return list.getTalk()->data;
+    }
 
     // Getter
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override{
+        return list.getCount();
+    }
 };
 
 
--- LLS.hpp
+++ LLS.hpp
@@ -11,17 +11,35 @@ private:
     LinkedList<T> list;
 public:
     // Constructor
-    LLS();
+    LLS() = default;
 
     // Insertion
-    void push(const T& item) override;
+    void push(const T& item) override{
+        list.addHead(item);
+    }
 
     // Deletion
-    T pop() override;
+    T pop() override{
+    if(list.getHead() == nullptr){
+            throw std::runtime_error("Empty");
+        }
+        
+        T value = list.getTail()->data;
+        list.removeTail();
+        return value;
+    }
 
     // Access
-    T peek() const override;
+    T peek() const override{
+         if(list.getHead() == nullptr){
+            throw std::runtime_error("Empty");
+        }
+
+        return list.getTail()->data;
+    }
 
     //Getters
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override{
+        return list.getCount();
+    }
 };
