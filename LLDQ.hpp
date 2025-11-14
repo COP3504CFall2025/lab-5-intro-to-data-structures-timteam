36c6df7db5634d82a41531c25756bc3314b51099
Copetim1 <twschoolb@gmail.com>
2025-11-14 18:36:49 +0000

    Added error checking for front and back functions for LLDQ

--- LLDQ.hpp
+++ LLDQ.hpp
@@ -48,9 +48,15 @@ public:
 
     // Element Accessors
     const T& front() const override{
+        if(list.getHead== nullptr){
+            throw std::runtime_error("Empty");
+        }
         return list.getHead()->data;
     }
     const T& back() const override{
+        if(list.getHead== nullptr){
+            throw std::runtime_error("Empty");
+        }
         return list.getTail()->data;
     }
 
